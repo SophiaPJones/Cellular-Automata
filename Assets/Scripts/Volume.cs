bbe7c9392dee7317e1b03b53289d5e779d669518
SkyeErnst <Tarnthelos@gmail.com>
2019-12-16 22:26:35 -0500

    More style changes, converting random generation to non-deprecated method.

--- Assets/Scripts/Volume.cs
+++ Assets/Scripts/Volume.cs
@@ -4,19 +4,10 @@
 
 
 using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Threading;
 using Aura2API;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.UI;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.UIElements;
-using Random = UnityEngine.Random;
 
 public class Volume : MonoBehaviour
 {   
@@ -25,22 +16,22 @@ public class Volume : MonoBehaviour
     /// Defines a set of actions that a cell can experience
     /// </summary>
     public enum CellActionID {Destroy, Create, Idle, IgnorePos}
-    
+
     /// <summary>
     /// A Cell action can be described with the position of the cell,
     /// and the the action that needs to occur
     /// </summary>
-    public struct CellAction 
+    private struct CellAction 
     {
         /// <summary>
         /// The position of the cell inside the grid
         /// </summary>
-        public Vector3 position;
+        public readonly Vector3 position;
 
         /// <summary>
         /// Action that should be applied to the cell
         /// </summary>
-        public CellActionID action;
+        public readonly CellActionID action;
 
         public CellAction(Vector3 position, CellActionID action)
         {
@@ -55,8 +46,8 @@ public class Volume : MonoBehaviour
     public float yUnit = 1.0f;
     public float zUnit = 1.0f;
     public RuleSet ruleSet;
-    private GameObject cellsWithin;
-    private CellularAutomata cellularAutomata;
+    private GameObject _cellsWithin;
+    private CellularAutomata _cellularAutomata;
     public delegate void CellRuleSimple(byte neighborCount);
     public bool simpleCellRule = false;
 
@@ -68,20 +59,22 @@ public class Volume : MonoBehaviour
     private void Start()
     {
         //The "Space" object has a "Volume" component and a "CellularAutomata" component
-        cellsWithin = GameObject.Find("Space"); 
-        cellularAutomata = cellsWithin.GetComponent<CellularAutomata>();
+        _cellsWithin = GameObject.Find("Space"); 
+        _cellularAutomata = _cellsWithin.GetComponent<CellularAutomata>();
+        
+        System.Random rand = new System.Random();
 
         for (int i = 0; i < 150; i++)
         {
             // Spawns a bunch of random cells in a 23x23x23 volume.
-            var randX = Random.RandomRange(0, 23);
-            var randY = Random.RandomRange(0, 23);
-            var randZ = Random.RandomRange(0, 23);
+            int randX = rand.Next(0, 23);
+            int randY = rand.Next(0, 23);
+            int randZ = rand.Next(0, 23);
             AddCell(new Vector3(randX,randY,randZ));
         }
     }
 
-    public void AddCell(Vector3 position)
+    private void AddCell(Vector3 position)
     {
         var cellObj = GameObject.Instantiate(cellPrefab,
             position,
@@ -90,18 +83,19 @@ public class Volume : MonoBehaviour
             // than creating entirely new object, especially since the prefab should have GPU instancing enabled.
             GameObject.FindGameObjectWithTag("Space").transform); 
         
-        cellObj.SetActive(true); //Set the object as active; no point in creating the object if the playing ain't gonna be able to see it.
+        //Set the object as active; no point in creating the object if the playing ain't gonna be able to see it.
+        cellObj.SetActive(true);
         
-        _interestingCells.AddOrUpdate(position, //We record the position of this cell in the _interestingCells dictionary
-                            true, //and set the value to "true", meaning that there is an active cell at that position.
-                    (oldKey, oldValue) => { return oldValue;}); //If for whatever reason we're unable to do that, just use the old value.
-        _cells.AddOrUpdate(position, cellObj, (oldKey, oldVal) => { return oldVal;}); //Store the actual cell in a seperate dictionary, also using the position as the key.
+        _interestingCells.AddOrUpdate(position, // We record the position of this cell in the _interestingCells dictionary,
+                            true, // and set the value to "true", meaning that there is an active cell at that position.
+                    (oldKey, oldValue) => { return oldValue;}); // If for whatever reason we're unable to do that, just use the old value.
+        _cells.AddOrUpdate(position, cellObj, (oldKey, oldVal) => oldVal); // Store the actual cell in a seperate dictionary, also using the position as the key.
         
-        for (var x = position.x - xUnit; x <= position.x + 1; x += xUnit)
+        for (float x = position.x - xUnit; x <= position.x + 1; x += xUnit)
         {
-            for (var y = position.y - yUnit; y <= position.y + 1; y += yUnit)
+            for (float y = position.y - yUnit; y <= position.y + 1; y += yUnit)
             {
-                for (var z = position.z - zUnit; z <= position.z + 1; z += zUnit)
+                for (float z = position.z - zUnit; z <= position.z + 1; z += zUnit)
                 {
                     if (x != position.x && y != position.y && z != position.z)
                     { 
@@ -117,21 +111,20 @@ public class Volume : MonoBehaviour
         }
     }
 
-    public void RemoveCell(Vector3 position)
+    private void RemoveCell(Vector3 position)
     {
         // Stores the instanced game object.
-        GameObject gameObj; 
         // We set that position to inactive in the _interestingCells dictionary
         // We still want to consider this cell for the future, because it may be bordering some other cell that will later become active,
         // meaning this cell may need to be referenced again.
         _interestingCells.AddOrUpdate(position, false, (oldkey, oldval) => { return oldval;}); 
 
         //Remove the game object, then destroy it.
-        _cells.TryRemove(position, out gameObj); 
+        _cells.TryRemove(position, out var gameObj); 
         gameObj.Destroy();
     }
     
-    public CellAction EvaluatePoint(Vector3 position)
+    private CellAction EvaluatePoint(Vector3 position)
     {
         byte neighborCount = 0;
         int  neighbors = 0;
@@ -154,24 +147,23 @@ public class Volume : MonoBehaviour
         */
         bool isCellOccupied = _cells.ContainsKey(position);
 
-        for (var x = position.x - xUnit; x <= position.x + xUnit; x++)
+        for (float x = position.x - xUnit; x <= position.x + xUnit; x++)
         {
-            for (var y = position.x - yUnit; y <= position.y + yUnit; y++)
+            for (float y = position.x - yUnit; y <= position.y + yUnit; y++)
             {
-                for (var z = position.z - zUnit; z <= position.z + zUnit; z++)
+                for (float z = position.z - zUnit; z <= position.z + zUnit; z++)
                 {
                     var vec = new Vector3(x, y, z);
                     if(_interestingCells.ContainsKey(vec))
                     {
                         if (position != vec)
                         {
-                            //set this cell's bit to 1;
-                            neighbors++; 
+                            // set this cell's bit to 1;
+                            neighbors++;
                             neighborCount++;
                         }
-                            
                     }
-                    //shift bit to the left;
+                    // shift bit to the left;
                     neighbors <<= 1; 
                 }
             }
@@ -189,7 +181,7 @@ public class Volume : MonoBehaviour
 
     public void DoStep()
     {
-        //We create a list of actions that will accumulate as the cellular automata resolves the step.
+        // We create a list of actions that will accumulate as the cellular automata resolves the step.
         // We do this because we do not want the state of the volume to change while we're checking all of the cells.
         // if that happens, then when we run CellRule to determine what to do to each cell, changes made to other cells
         // will affect the behavior of different cells in the same step.
@@ -226,6 +218,10 @@ public class Volume : MonoBehaviour
                     });
                     Debug.Log($"Cell IGNORED at {pos}.");
                     break;
+                case CellActionID.Idle:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
